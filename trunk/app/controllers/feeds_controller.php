7c9412ddb469594e6311da11fb14d5ff2b73a142
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2007-09-19 19:11:51 +0000

    solves issue #29 - moved shell_dispatcher to app/webroot/ - enhanced caching refresh to be not afraid of two tasks running parallel

--- trunk/app/controllers/feeds_controller.php
+++ trunk/app/controllers/feeds_controller.php
@@ -31,24 +31,37 @@ class FeedsController extends AppController {
     /**
      * Refreshes 250 feed caches, ordered by priority. Priority
      * is increased for every access to a feed.
-     * Priority is set to 0 before each refresh to avoid endless
-     * loops, when there is a problem with a specific feed.
      *
      * @param  
      * @return 
      */
     public function shell_refresh() {
-        $this->Feed->recursive = 1;
-        $this->Feed->expects('Feed.Feed', 'Feed.Account', 'Account.Account');
-        $data = $this->Feed->findAll(null, null, 'Feed.priority DESC', 250);
-        
         $refreshed = array();
-        foreach($data as $item) {
-            $feed_data = $this->Feed->Account->Service->feed2array($item['Account']['service_id'], $item['Account']['feed_url'], 10, false);
-            $this->Feed->store($item['Account']['id'], $feed_data);
-            $refreshed[] = $item['Account']['feed_url'];
-        }
+
+        # I do this like this and not through a LIMIT of 250, because
+        # then more than one task could run at once, without doing any harm
+        for($i=0; $i<250; $i++) {
+            # now two refresh's within 30 minutes
+            $last_refresh = date('Y-m-d H:i:s', strtotime('-30 minutes'));
+            
+            $this->Feed->recursive = 1;
+            $this->Feed->expects('Feed.Feed', 'Feed.Account', 'Account.Account');
+            $data = $this->Feed->findAll(array('Feed.modified < "' . $last_refresh . '"'), null, 'Feed.modified ASC, Feed.priority DESC', 1);
         
+            foreach($data as $item) {
+                # set the modified right now, so a parallel running task
+                # would not get it, while we are fetching the feed
+                $this->Feed->id = $item['Feed']['id'];
+                $this->Feed->saveField('modified', date('Y-m-d H:i:s'));
+                
+                # get the actual feed. Maximum of 10 items, but without time restriction
+                $feed_data = $this->Feed->Account->Service->feed2array($item['Account']['service_id'], $item['Account']['feed_url'], 10, false);
+                
+                # save it to the cache
+                $this->Feed->store($item['Account']['id'], $feed_data);
+                $refreshed[] = $item['Account']['feed_url'];
+            }
+        }
         $this->set('data', $refreshed);
     }
     
