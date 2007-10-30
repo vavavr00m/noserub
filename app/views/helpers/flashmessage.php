fbd692924ab19b9440b49415d0b8d7a411560939
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2007-10-30 20:21:29 +0000

    fixed a small bug that prevents this helper from raising errors.

--- app/views/helpers/flashmessage.php
+++ app/views/helpers/flashmessage.php
@@ -8,12 +8,14 @@ class FlashMessageHelper extends AppHelper {
     public function render() {
         $flash_messages = $this->Session->read('FlashMessages');
         
-        foreach($flash_messages as $type => $messages) {
-            echo '<div id="message" class="' . $type . '">';
-            foreach($messages as $message) {
-                echo '<p>' . $message . '</p>';
+        if($flash_messages) {
+            foreach($flash_messages as $type => $messages) {
+                echo '<div id="message" class="' . $type . '">';
+                foreach($messages as $message) {
+                    echo '<p>' . $message . '</p>';
+                }
+                echo '</div>';
             }
-            echo '</div>';
         }
     }
 
