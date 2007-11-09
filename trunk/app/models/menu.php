52d51dc34a6f869e5d90cabd824801cbae09c951
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2007-11-09 14:14:47 +0000

    - Many refactorings - Menu no longer a Cake model

--- trunk/app/models/menu.php
+++ trunk/app/models/menu.php
@@ -1,7 +1,6 @@
 <?php
 
-	class Menu extends AppModel {
-		var $useTable = false;
+	class Menu {
 		
 		function getMainMenu($options) {
 			$menuItems = array();
@@ -10,7 +9,8 @@
 			
 			if (isset($options['is_local'])) {
 				if ($options['is_local'] === true) {
-					$menuItems = $this->getMainMenuForLocalUser($controller, $action);
+					$localUsername = isset($options['local_username']) ? $options['local_username'] : '';
+					$menuItems = $this->getMainMenuForLocalUser($controller, $action, $localUsername);
 				} else {
 					$menuItems = $this->getMainMenuForRemoteUser($controller, $action);
 				}
@@ -22,27 +22,27 @@
 		}
 		
 		private function getMainMenuForAnonymousUser($controller, $action, $registrationType) {
-			$menuItems[] = new MenuItem('Social Stream', '/social_stream/', $this->shouldSocialStreamBeActivated($controller, $action));
+			$menuItems[] = new SocialStreamMenuItem($controller, $action);
 			
 			if ($registrationType == 'all') {
-				$menuItems[] = new MenuItem('Add me!', '/pages/register/', $this->shouldRegisterBeActivated($controller, $action));
+				$menuItems[] = new RegisterMenuItem($controller, $action);
 			}
 			
 			return $menuItems;
 		}
 		
-		private function getMainMenuForLocalUser($controller, $action) {
-			$menuItems[] = new MenuItem('Social Stream', '/social_stream/', $this->shouldSocialStreamBeActivated($controller, $action));
-			$menuItems[] = new MenuItem('My Profile', '', $this->shouldMyProfileBeActivated($controller, $action));
-			$menuItems[] = new MenuItem('My Contacts', '', $controller == 'Contacts');
-			$menuItems[] = new MenuItem('Settings', '', $this->shouldSettingsBeActivated($controller, $action));
+		private function getMainMenuForLocalUser($controller, $action, $localUsername) {
+			$menuItems[] = new SocialStreamMenuItem($controller, $action);
+			$menuItems[] = new MyProfileMenuItem($controller, $action, $localUsername);
+			$menuItems[] = new MyContactsMenuItem($controller, $localUsername);
+			$menuItems[] = new SettingsMenuItem($controller, $action, $localUsername);
 			
 			return $menuItems;
 		}
 		
 		private function getMainMenuForRemoteUser($controller, $action) {
-			$menuItems[] = new MenuItem('Social Stream', '/social_stream/', $this->shouldSocialStreamBeActivated($controller, $action));
-			$menuItems[] = new MenuItem('My Profile', '', $this->shouldMyProfileBeActivated($controller, $action));
+			$menuItems[] = new SocialStreamMenuItem($controller, $action);
+			$menuItems[] = new MyProfileMenuItem($controller, $action);
 			
 			return $menuItems;
 		}
@@ -57,48 +57,6 @@
 			}
 			
 			return $registrationType;
-		}
-		
-		private function shouldMyProfileBeActivated($controller, $action) {
-			if ($controller == 'Identities' && $action == 'index') {
-				return true;
-			}
-			
-			return false;
-		}
-		
-		private function shouldRegisterBeActivated($controller, $action) {
-			if ($controller == 'Identities' && $action == 'register') {
-				return true;
-			}
-			
-			return false;
-		}
-		
-		private function shouldSettingsBeActivated($controller, $action) {
-			$controllers = array('Accounts', 'OpenidSites', 'Syndications');
-			
-			if (in_array($controller, $controllers)) {
-				return true;
-			}
-				
-			if ($controller == 'Identities') {
-				$identityActions = array('account_settings', 'password_settings', 'privacy_settings', 'profile_settings');
-					
-				if (in_array($action, $identityActions)) {
-					return true;
-				}
-			}
-			
-			return false;
-		}
-		
-		private function shouldSocialStreamBeActivated($controller, $action) {
-			if ($controller == 'Identities' && $action == 'social_stream') {
-				return true;
-			}
-			
-			return false;
 		}
 	}
 	
@@ -123,6 +81,112 @@
 		
 		function isActive() {
 			return $this->isActive;
+		}
+	}
+	
+	class MyContactsMenuItem extends MenuItem {
+		private $controller = null;
+		
+		function __construct($controller, $localUsername) {
+			parent::__construct('My Contacts', '/' . $localUsername . '/contacts/', false);
+			$this->controller = $controller;
+		}
+		
+		function isActive() {
+			if ($this->controller == 'Contacts') {
+				return true;
+			}
+			
+			return false;
+		}
+	}
+	
+	class MyProfileMenuItem extends MenuItem {
+		private $controller = null;
+		private $action = null;
+		
+		function __construct($controller, $action, $localUsername = null) {
+			// TODO adding link for profile of remote user
+			$link = ($localUsername == null) ? '' : '/' . $localUsername . '/';
+			
+			parent::__construct('My Profile', $link, false);
+			$this->controller = $controller;
+			$this->action = $action;
+		}
+		
+		function isActive() {
+			if ($this->controller == 'Identities' && $this->action == 'index') {
+				return true;
+			}
+			
+			return false;
+		}
+	}
+	
+	class RegisterMenuItem extends MenuItem {
+		private $controller = null;
+		private $action = null;
+		
+		function __construct($controller, $action) {
+			parent::__construct('Add me!', '/pages/register/', false);
+			$this->controller = $controller;
+			$this->action = $action;
+		}
+		
+		function isActive() {
+			if ($this->controller == 'Identities' && $this->action == 'register') {
+				return true;
+			}
+			
+			return false;
+		}
+	}
+	
+	class SettingsMenuItem extends MenuItem {
+		private $controller = null;
+		private $action = null;
+		
+		function __construct($controller, $action, $localUsername) {
+			parent::__construct('Settings', '/' . $localUsername . '/settings/', false);
+			$this->controller = $controller;
+			$this->action = $action;
+		}
+		
+		function isActive() {
+			$controllers = array('Accounts', 'OpenidSites', 'Syndications');
+			
+			if (in_array($this->controller, $controllers)) {
+				return true;
+			}
+			
+			if ($this->controller == 'Identities') {
+				$identityActions = array('account_settings', 'password_settings', 'privacy_settings', 'profile_settings');
+					
+				if (in_array($this->action, $identityActions)) {
+					return true;
+				}
+			}
+			
+			return false;
+		}
+	}
+	
+	class SocialStreamMenuItem extends MenuItem {
+		private $controller = null;
+		private $action = null;
+		
+		function __construct($controller, $action) {
+			parent::__construct('Social Stream', '/social_stream/', false);
+			$this->controller = $controller;
+			$this->action = $action;
+		}
+		
+		function isActive() {
+			if ($this->controller == 'Identities' && $this->action == 'social_stream') {
+				return true;
+			}
+			
+			return false;
 		}
 	}
 ?>
--- trunk/app/tests/models/menu_test.php
+++ trunk/app/tests/models/menu_test.php
@@ -10,40 +10,40 @@
 		// local user
 		
 		function testGetMainMenuForLocalUser() {
-			$mainMenu = $this->menu->getMainMenu(array('is_local' => true));
+			$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'local_username' => 'test'));
 			$this->assertEqual(4, count($mainMenu));
-			$this->assertMenuForLocalUser($mainMenu, false, false, false, false);
+			$this->assertMenuForLocalUser($mainMenu, 'test', false, false, false, false);
 		}
 		
 		function testGetMainMenuWithMyContactsSelected() {
-			$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => 'Contacts'));
-			$this->assertMenuForLocalUser($mainMenu, false, false, true, false);
+			$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => 'Contacts', 'local_username' => 'test'));
+			$this->assertMenuForLocalUser($mainMenu, 'test', false, false, true, false);
 		}
 		
 		function testGetMainMenuWithMyProfileSelected() {
-			$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => 'Identities', 'action' => 'index'));
-			$this->assertMenuForLocalUser($mainMenu, false, true, false, false);
+			$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => 'Identities', 'action' => 'index', 'local_username' => 'test'));
+			$this->assertMenuForLocalUser($mainMenu, 'test', false, true, false, false);
 		}
 		
 		function testGetMainMenuWithSettingsSelected() {
 			$controllers = array('Accounts', 'OpenidSites', 'Syndications');
 			
 			foreach ($controllers as $controller) {
-				$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => $controller));
-				$this->assertMenuForLocalUser($mainMenu, false, false, false, true);
+				$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => $controller, 'local_username' => 'test'));
+				$this->assertMenuForLocalUser($mainMenu, 'test', false, false, false, true);
 			}
 			
 			$identityActions = array('account_settings', 'password_settings', 'privacy_settings', 'profile_settings');
 			
 			foreach ($identityActions as $action) {
-				$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => 'Identities', 'action' => $action));
-				$this->assertMenuForLocalUser($mainMenu, false, false, false, true);
+				$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => 'Identities', 'action' => $action, 'local_username' => 'test'));
+				$this->assertMenuForLocalUser($mainMenu, 'test', false, false, false, true);
 			}
 		}
 		
 		function testGetMainMenuWithSocialStreamSelected() {
-			$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => 'Identities', 'action' => 'social_stream'));
-			$this->assertMenuForLocalUser($mainMenu, true, false, false, false);
+			$mainMenu = $this->menu->getMainMenu(array('is_local' => true, 'controller' => 'Identities', 'action' => 'social_stream', 'local_username' => 'test'));
+			$this->assertMenuForLocalUser($mainMenu, 'test', true, false, false, false);
 		}		
 		
 		// remote user
@@ -51,8 +51,8 @@
 		function testGetMainMenuForRemoteUser() {
 			$mainMenu = $this->menu->getMainMenu(array('is_local' => false));
 			$this->assertEqual(2, count($mainMenu));
-			$this->assertMenuItem($mainMenu[0], 'Social Stream', false);
-			$this->assertMenuItem($mainMenu[1], 'My Profile', false);
+			$this->assertMenuItem($mainMenu[0], 'Social Stream', '/social_stream/', false);
+			$this->assertMenuItem($mainMenu[1], 'My Profile', '', false);
 		}
 		
 		// anonymous user
@@ -60,31 +60,151 @@
 		function testGetMainMenuForWebUsersWithRegistrationPossibility() {
 			$mainMenu = $this->menu->getMainMenu(array('registration_type' => 'all'));
 			$this->assertEqual(2, count($mainMenu));
-			$this->assertMenuItem($mainMenu[0], 'Social Stream', false);
-			$this->assertMenuItem($mainMenu[1], 'Add me!', false);
+			$this->assertMenuItem($mainMenu[0], 'Social Stream', '/social_stream/', false);
+			$this->assertMenuItem($mainMenu[1], 'Add me!', '/pages/register/', false);
 		}
 
 		function testGetMainMenuWithRegisterSelected() {
 			$mainMenu = $this->menu->getMainMenu(array('registration_type' => 'all', 'controller' => 'Identities', 'action' => 'register'));
-			$this->assertMenuItem($mainMenu[0], 'Social Stream', false);
-			$this->assertMenuItem($mainMenu[1], 'Add me!', true);
+			$this->assertMenuItem($mainMenu[0], 'Social Stream', '/social_stream/', false);
+			$this->assertMenuItem($mainMenu[1], 'Add me!', '/pages/register/', true);
 		}
 		
 		function testGetMainMenuForWebUsersWithoutRegistrationPossibility() {
 			$mainMenu = $this->menu->getMainMenu(array('registration_type' => 'none'));
 			$this->assertEqual(1, count($mainMenu));
-			$this->assertMenuItem($mainMenu[0], 'Social Stream', false);
+			$this->assertMenuItem($mainMenu[0], 'Social Stream', '/social_stream/', false);
 		}
 		
-		private function assertMenuForLocalUser($mainMenu, $socialStreamActive, $myProfileActive, $myContactsActive, $settingsActive) {
-			$this->assertMenuItem($mainMenu[0], 'Social Stream', $socialStreamActive);
-			$this->assertMenuItem($mainMenu[1], 'My Profile', $myProfileActive);
-			$this->assertMenuItem($mainMenu[2], 'My Contacts', $myContactsActive);
-			$this->assertMenuItem($mainMenu[3], 'Settings', $settingsActive);
+		private function assertMenuForLocalUser($mainMenu, $localUsername, $socialStreamActive, $myProfileActive, $myContactsActive, $settingsActive) {
+			$this->assertMenuItem($mainMenu[0], 'Social Stream', '/social_stream/', $socialStreamActive);
+			$this->assertMenuItem($mainMenu[1], 'My Profile', '/' . $localUsername . '/', $myProfileActive);
+			$this->assertMenuItem($mainMenu[2], 'My Contacts', '/' . $localUsername . '/contacts/', $myContactsActive);
+			$this->assertMenuItem($mainMenu[3], 'Settings', '/' . $localUsername . '/settings/',$settingsActive);
 		}
 		
-		private function assertMenuItem(MenuItem $menuItem, $label, $isActive) {
+		private function assertMenuItem(MenuItem $menuItem, $label, $link, $isActive) {
 			$this->assertEqual($label, $menuItem->getLabel());
+			$this->assertEqual($link, $menuItem->getLink());
+			$this->assertIdentical($isActive, $menuItem->isActive());
+		}
+	}
+	
+	class MenuitemTest extends CakeTestCase {
+		function testMenuItem() {
+			$label = 'A menu item';
+			$link = '/test';
+			$isActive = true;
+			
+			$menuItem = new MenuItem($label, $link, $isActive);
+			$this->assertEqual($label, $menuItem->getLabel());
+			$this->assertEqual($link, $menuItem->getLink());
+			$this->assertIdentical($isActive, $menuItem->isActive());
+		}
+	}
+	
+	class MyContactsMenuItemTest extends CakeTestCase {
+		function testCreateActivatedMyContactsMenuItem() {
+			$menuItem = new MyContactsMenuItem('Contacts', 'testuser');
+			$this->assertMenuItem($menuItem, '/testuser/contacts/', true);
+		}
+		
+		function testCreateNotActivatedMyContactsMenuItem() {
+			$menuItem = new MyContactsMenuItem('SomeController', 'test');
+			$this->assertMenuItem($menuItem, '/test/contacts/', false);
+		}
+		
+		private function assertMenuItem($menuItem, $link, $isActive) {
+			$this->assertEqual('My Contacts', $menuItem->getLabel());
+			$this->assertEqual($link, $menuItem->getLink());
+			$this->assertIdentical($isActive, $menuItem->isActive());
+		}
+	}
+	
+	class MyProfileMenuItemTest extends CakeTestCase {
+		function testCreateActivatedMyProfileMenuItem() {
+			$menuItem = new MyProfileMenuItem('Identities', 'index', 'testuser');
+			$this->assertMenuItem($menuItem, '/testuser/', true);
+		}
+		
+		function testCreateNotActivatedMyProfileMenuItem() {
+			$menuItem = new MyProfileMenuItem('SomeController', 'someaction', 'test');
+			$this->assertMenuItem($menuItem, '/test/', false);
+		}
+		
+		function testCreateMyProfileMenuItemWithoutUsername() {
+			$menuItem = new MyProfileMenuItem('SomeController', 'someaction');
+			$this->assertMenuItem($menuItem, '', false);
+		}
+		
+		private function assertMenuItem($menuItem, $link, $isActive) {
+			$this->assertEqual('My Profile', $menuItem->getLabel());
+			$this->assertEqual($link, $menuItem->getLink());
+			$this->assertIdentical($isActive, $menuItem->isActive());
+		}
+	}
+	
+	class RegisterMenuItemTest extends CakeTestCase {
+		function testCreateActivatedRegisterMenuItem() {
+			$menuItem = new RegisterMenuItem('Identities', 'register');
+			$this->assertMenuItem($menuItem, true);
+		}
+		
+		function testCreateNotActivatedRegisterMenuItem() {			
+			$menuItem = new RegisterMenuItem('SomeController', 'someaction');
+			$this->assertMenuItem($menuItem, false);
+		}
+		
+		private function assertMenuItem($menuItem, $isActive) {
+			$this->assertEqual('Add me!', $menuItem->getLabel());
+			$this->assertEqual('/pages/register/', $menuItem->getLink());
+			$this->assertIdentical($isActive, $menuItem->isActive());
+		}
+	}
+	
+	class SettingsMenuItemTest extends CakeTestCase {
+		function testCreateActivatedSettingsMenuItem() {
+			$controllers = array('Accounts', 'OpenidSites', 'Syndications');
+			
+			foreach ($controllers as $controller) {
+				$menuItem = new SettingsMenuItem($controller, '', 'testuser');
+				$this->assertMenuItem($menuItem, '/testuser/settings/', true);
+			}
+			
+			$identityActions = array('account_settings', 'password_settings', 'privacy_settings', 'profile_settings');
+			
+			foreach ($identityActions as $action) {
+				$menuItem = new SettingsMenuItem('Identities', $action, 'user');
+				$this->assertMenuItem($menuItem, '/user/settings/', true);
+			}
+		}
+		
+		function testCreateNotActivatedSettingsMenuItem() {
+			$menuItem = new SettingsMenuItem('SomeController', 'someaction', 'test');
+			$this->assertMenuItem($menuItem, '/test/settings/', false);
+		}
+		
+		private function assertMenuItem($menuItem, $link, $isActive) {
+			$this->assertEqual('Settings', $menuItem->getLabel());
+			$this->assertEqual($link, $menuItem->getLink());
+			$this->assertIdentical($isActive, $menuItem->isActive());
+		}
+	}
+	
+	class SocialStreamMenuItemTest extends CakeTestCase {
+		function testCreateActivatedSocialStreamMenuItem() {
+			$menuItem = new SocialStreamMenuItem('Identities', 'social_stream');
+			$this->assertMenuItem($menuItem, true);
+		}
+		
+		function testCreateNotActivatedSocialStreamMenuItem() {			
+			$menuItem = new SocialStreamMenuItem('SomeController', 'someaction');
+			$this->assertMenuItem($menuItem, false);
+		}
+		
+		private function assertMenuItem($menuItem, $isActive) {
+			$this->assertEqual('Social Stream', $menuItem->getLabel());
+			$this->assertEqual('/social_stream/', $menuItem->getLink());
 			$this->assertIdentical($isActive, $menuItem->isActive());
 		}
 	}
