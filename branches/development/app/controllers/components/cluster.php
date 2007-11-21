99888c563d998598024e36ae7ff4ee3f306e00ea
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2007-11-21 22:28:59 +0000

    for social stream: don't show future days

--- branches/development/app/controllers/components/cluster.php
+++ branches/development/app/controllers/components/cluster.php
@@ -8,13 +8,18 @@ class ClusterComponent extends Object {
      * Clusters will by YYY-MM-DD DESC
      *
      * @param  array $data
+     * @param  bool $with_future_dates 
      * @return array $clustered_data
      * @access 
      */
-    public function create($data) {
+    public function create($data, $with_future_dates = false) {
+        $today = date('Y-m-d');
         $clustered_data = array();
         foreach($data as $key => $value) {
             $day = date('Y-m-d', strtotime($value['datetime']));
+            if($with_future_dates === false && $day > $today) {
+                continue;
+            }
             if(!isset($clustered_data[$day])) {
                 $clustered_data[$day] = array();
             }
