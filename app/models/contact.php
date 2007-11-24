10a3dd6fac163441893193b9f4f8028246ac30b8
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2007-11-24 13:28:33 +0000

    Fixing typo

--- app/models/contact.php
+++ app/models/contact.php
@@ -4,7 +4,7 @@
 class Contact extends AppModel {
     var $belongsTo = array('Identity',
                            'WithIdentity' => array('className' => 'Identity',
-                                                   'foreinKey' => 'with_identity_id'));
+                                                   'foreignKey' => 'with_identity_id'));
                                                    
     var $validate = array(
             'username' => array('content'  => array('rule' => array('custom', NOSERUB_VALID_USERNAME)),
