968cf1563b3462aa0ea34d324c435d086bf9862c
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2007-11-24 13:44:49 +0000

    Fixing bugs in migration file

--- branches/development/app/config/sql/migrations/0000051_add_contact_type_tables.sql
+++ branches/development/app/config/sql/migrations/0000051_add_contact_type_tables.sql
@@ -1,7 +1,4 @@
 CREATE TABLE `contact_types` (`id` int(11) unsigned NOT NULL auto_increment, `identity_id` int(11) unsigned NOT NULL, `name` varchar(255) NOT NULL,`created` datetime NOT NULL,`modified` datetime NOT NULL,PRIMARY KEY (`id`)) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
-CREATE TABLE `contacts_contact_types` (`contact_id` int(11) unsigned NOT NULL,`contact_type_id` int(11) unsigned NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
+CREATE TABLE `contact_types_contacts` (`contact_id` int(11) unsigned NOT NULL,`contact_type_id` int(11) unsigned NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 CREATE TABLE `noserub_contact_types` (`id` int(11) unsigned NOT NULL auto_increment, `name` varchar(255) NOT NULL,PRIMARY KEY (`id`)) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
 CREATE TABLE `contacts_noserub_contact_types` (`contact_id` int(11) unsigned NOT NULL,`noserub_contact_type_id` int(11) unsigned NOT NULL) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
