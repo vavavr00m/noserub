b91225d4439eaa56f8382b007d739340088cfa23
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2007-11-28 15:25:59 +0000

    - Allowing definition of user-defined contact types when creating a contact - Showing user-defined contact types on edit page (no editing yet)

--- branches/development/app/models/contact_type.php
+++ branches/development/app/models/contact_type.php
@@ -1,6 +1,41 @@
 <?php
 
+uses('Sanitize');
+
 class ContactType extends AppModel {
 	var $hasAndBelongsToMany = array('Contact');
+	
+	function getIds($identityId, $contactTypes) {
+		$sanitizedContactTypes = $this->sanitizeContactTypes($contactTypes);
+
+		$this->expects('ContactType');
+		$ids = $this->findAll(array('ContactType.identity_id' => $identityId, 'ContactType.name' => $sanitizedContactTypes), 'ContactType.id');
+		$ids = Set::extract($ids, '{n}.ContactType.id');
+		
+		return $ids;
+	}
+	
+	function saveIfNotExisting($identityId, $contactTypes) {
+		$sanitizedContactTypes = $this->sanitizeContactTypes($contactTypes);
+		
+		foreach ($sanitizedContactTypes as $contactType) {
+			if (!$this->hasAny(array('ContactType.identity_id' => $identityId, 'ContactType.name' => $contactType))) {
+				$data['ContactType']['identity_id'] = $identityId;
+				$data['ContactType']['name'] = $contactType;
+				$this->create($data);
+				$this->save();
+			}
+		}
+	}
+	
+	private function sanitizeContactTypes($contactTypes) {
+		$sanitized = array();
+		
+		foreach ($contactTypes as $contactType) {
+			$sanitized[] = low(Sanitize::paranoid($contactType));
+		}
+		
+		return $sanitized;
+	}
 }
 ?>
