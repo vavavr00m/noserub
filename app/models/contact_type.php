f9fb517fcd580e999e4ecb8f1939e4362ea9c090
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2007-12-01 14:02:30 +0000

    Refactoring and adding tests

--- app/models/contact_type.php
+++ app/models/contact_type.php
@@ -5,6 +5,16 @@ uses('Sanitize');
 class ContactType extends AppModel {
 	var $hasAndBelongsToMany = array('Contact');
 	
+	function createContactTypes($identityId, $contactTypes) {
+		$data['ContactType']['identity_id'] = $identityId;
+		
+		foreach ($contactTypes as $contactType) {
+			$data['ContactType']['name'] = $contactType;
+			$this->create($data);
+			$this->save();
+		}
+	}
+	
 	function getContactTypesFromString($string) {
 		if (empty($string)) {
 			return array();
@@ -13,6 +23,34 @@ class ContactType extends AppModel {
 		return array_unique(explode(' ', $string));
 	}
 	
+	function getIDsOfContactTypes($identityId, $contactTypes) {
+		$this->expects('ContactType');
+		$contactTypeIDs = $this->findAll(array('ContactType.identity_id' => $identityId, 'ContactType.name' => $contactTypes), 'ContactType.id');
+		$contactTypeIDs = Set::extract($contactTypeIDs, '{n}.ContactType.id');
+		
+		return $contactTypeIDs;
+	}
+	
+	/**
+	 * Returns those contact types which are not already in the database.
+	 */
+	function getNewContactTypes($identityId, $contactTypes) {
+		$newContactTypes = array();
+		
+		$this->expects('ContactType');
+		$existingContactTypes = $this->findAll(array('ContactType.identity_id' => $identityId, 'ContactType.name' => $contactTypes));
+		$existingContactTypes = Set::extract($existingContactTypes, '{n}.ContactType.name');
+
+		foreach ($contactTypes as $contactType) {
+			if (!in_array($contactType, $existingContactTypes)) {
+				$newContactTypes[] = $contactType;
+			}
+		}
+		
+		return $newContactTypes;
+	}
+	
+	// TODO remove this function
 	function getIds($identityId, $contactTypes) {
 		$sanitizedContactTypes = $this->sanitizeContactTypes($contactTypes);
 
@@ -23,6 +61,7 @@ class ContactType extends AppModel {
 		return $ids;
 	}
 	
+	// TODO remove this function
 	function saveIfNotExisting($identityId, $contactTypes) {
 		$sanitizedContactTypes = $this->sanitizeContactTypes($contactTypes);
 		
@@ -36,6 +75,7 @@ class ContactType extends AppModel {
 		}
 	}
 	
+	// TODO remove this function
 	private function sanitizeContactTypes($contactTypes) {
 		$sanitized = array();
 		
