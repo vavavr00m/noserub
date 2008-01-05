9fffc934061c65c4607f44ab757a17e2b64b1303
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-01-05 17:45:47 +0000

    location changes now end up in activities and thus are shown in social stream.

---
+++ branches/development/app/models/activity.php
@@ -0,0 +1,54 @@
+<?php
+/* SVN FILE: $Id:$ */
+ 
+class Activity extends AppModel {
+    var $belongsTo = array('Identity', 'ServiceType');
+    
+    public function getLatest($identity_id, $filter) {
+        $items = array();
+        
+        if($filter == 'location' || $filter == '') {
+            $items = $this->getLocations($identity_id);
+        }
+        
+        return $items;
+    }
+    
+    public function getLocations($identity_id) {
+        $items = array();
+        
+        $this->recursive = 1;
+        $this->expects('Location', 'ServiceType', 'Identity');
+        $data = $this->findAllByIdentityId($identity_id);
+        
+        foreach($data as $activity) {
+            $item = array();
+		    $item['datetime'] = $activity['Activity']['created'];
+		    		
+		    $item['title']    = $activity['Activity']['content'];
+		    $item['url']      = '';
+            $item['intro']    = $activity['ServiceType']['intro'];
+            $item['type']     = $activity['ServiceType']['token'];
+            $item['username'] = $activity['Identity']['username'];
+            $item['content']  = $activity['Activity']['content'];
+            
+		    $items[] = $item;
+		}
+		
+        return $items;
+    }
+    
+    public function setLocation($identity_id, $location) {
+        if($location && isset($location['Location']['name']) && $location['Location']['name'] != '') {
+            $data = array('identity_id'     => $identity_id,
+                          'service_type_id' => 9,
+                          'content'         => $location['Location']['name'],
+                          'created'         => date('Y-m-d H:i:s'));
+                      
+            $this->create();
+            return $this->save($data);
+        } else {
+            return false;
+        }
+    }                                                   
+}
