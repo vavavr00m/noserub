1ffa07f4de49564a6589a2270dd4d41ff7d6a337
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-03-02 18:16:20 +0000

    - fixed security issue with API (user could set any location, not only his/her own) - added link to API documentation on noserub.com/documentation

--- app/models/location.php
+++ app/models/location.php
@@ -5,15 +5,19 @@ class Location extends AppModel {
     var $belongsTo = array('Identity');                                                   
 
     public function setTo($identity_id, $location_id) {
-        $data = array('last_location_id' => $location_id);
-        $this->Identity->id = $identity_id;
-        $this->Identity->save($data);
-        
-        # we need to get the complete location for the activity
-        $this->id = $location_id;
+        # check, that this location belongs to that identity
         $this->recursive = 0;
         $this->expects('Location');
         $location = $this->findById($location_id);
+        if($location['Location']['identity_id'] != $identity_id) {
+            return false;
+        }
+        
+        $this->Identity->id = $identity_id;
+        $this->Identity->saveField(last_location_id, $location_id);
+        
         $this->Identity->Activity->setLocation($identity_id, $location);
+
+        return true;
     }
 }
