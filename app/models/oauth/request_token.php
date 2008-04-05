5b2b36916ae1283a825da8df6dfde5ca5d1bc919
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-04-05 08:13:31 +0000

    Implementing access_token functionality

--- app/models/oauth/request_token.php
+++ app/models/oauth/request_token.php
@@ -6,5 +6,9 @@ class RequestToken extends AppModel {
 	public function authorize($token_key) {
 		$this->updateAll(array('authorized' => true), array('RequestToken.token_key' => $token_key));
 	}
+	
+	public function isAuthorized($token_key) {
+		return $this->hasAny(array('RequestToken.authorized' => true, 'RequestToken.token_key' => $token_key));
+	}
 }
 ?>
