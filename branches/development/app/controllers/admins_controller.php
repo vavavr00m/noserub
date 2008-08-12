94bf0ca556abb756be3b4b69e43c9f4a6fc3cf54
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-08-12 07:59:28 +0000

    Moving access control of AdminsController to beforeFilter, fixing issue with "invalid admin hash" error

--- branches/development/app/controllers/admins_controller.php
+++ branches/development/app/controllers/admins_controller.php
@@ -3,26 +3,23 @@
 class AdminsController extends AppController {
     public $uses = array('Admin');
     
-    /**
+    public function beforeFilter() {
+    	parent::beforeFilter();
+    	$admin_hash = isset($this->params['admin_hash']) ? $this->params['admin_hash'] : '';
+        
+        if($admin_hash != NOSERUB_ADMIN_HASH || $admin_hash == '') {
+            # there is nothing to do for us here
+            echo $this->render('/elements/hash_not_valid');
+			exit;
+        }
+    }
+
+	/**
      * Update the system. Check for new migrations
-     * and possible new constants.
-     *
-     * @param  string admin_hash (through $this->params)
-     * @return 
-     * @access 
+     * and possible new constants. 
      */
     public function system_update() {
-        $admin_hash = isset($this->params['admin_hash']) ? $this->params['admin_hash'] : '';
-        
-        if($admin_hash != NOSERUB_ADMIN_HASH ||
-           $admin_hash == '') {
-            # there is nothing to do for us here
-            $this->render('../elements/hash_not_valid');
-            exit;
-        }
-
-        $extensions = $this->Admin->checkExtensions();
-        $this->set('extensions', $extensions);
+        $this->set('extensions', $this->Admin->checkExtensions());
         
         $directories = $this->Admin->checkWriteable();
         $this->set('directories', $directories);
