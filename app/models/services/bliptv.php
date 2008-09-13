0cab4220adef49cdb08b8f902746e386e8d507c1
judocoach <judocoach@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-09-13 09:56:31 +0000

    Added BlipTV service

---
+++ app/models/services/bliptv.php
@@ -0,0 +1,24 @@
+<?php
+class BliptvService extends AbstractService {
+	
+	public function detectService($url) {
+		return $this->extractUsername($url, array('#(.+).blip.tv#'));
+	}
+	
+	public function getAccountUrl($username) {
+		return 'http://'.$username.'blip.tv/';
+	}
+	
+#	public function getContacts($username) {
+#		return ContactExtractor::getContactsFromSinglePage('http://www.viddler.com/explore/' . $username . '/friends/', '/<p><strong><a.*href="\/explore\/.*\/".*>(.*)<\/a>/iU');
+#	}
+	
+	public function getContent($feeditem) {
+		return $feeditem->get_content();
+	}
+	
+	public function getFeedUrl($username) {
+		return 'http://'.$username.'/blip.tv/rss/';
+	}
+}
+?>
