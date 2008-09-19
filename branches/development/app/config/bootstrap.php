beb9eedf028f2fe17a660765364ccc351894b411
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-09-19 15:07:31 +0000

    fixing typo in UserAgent

--- branches/development/app/config/bootstrap.php
+++ branches/development/app/config/bootstrap.php
@@ -51,7 +51,7 @@ if(file_exists(APP . '/config/noserub.php')) {
 }
 
 if(defined('NOSERUB_FULL_BASE_URL')) {
-    define('NOSERUB_USER_AGENT', 'NoseRub bot from ' . NOSERUB_FULL_BASE_URL . '(http://noserub.com/)');
+    define('NOSERUB_USER_AGENT', 'NoseRub bot from ' . NOSERUB_FULL_BASE_URL . ' (http://noserub.com/)');
 } else {
     define('NOSERUB_USER_AGENT', 'NoseRub bot from unknown host (http://noserub.com/)');
 }
