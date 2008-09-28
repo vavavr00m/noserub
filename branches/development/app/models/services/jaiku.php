3bf74efde6d8d71a2722c7674b021e4ea8a83736
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-09-28 16:58:37 +0000

    first draft to remove duplicates for an individual

--- branches/development/app/models/services/jaiku.php
+++ branches/development/app/models/services/jaiku.php
@@ -13,12 +13,6 @@ class JaikuService extends AbstractService {
 		return ContactExtractor::getContactsFromSinglePage('http://'.$username.'.jaiku.com', '/<a href="http:\/\/(.*).jaiku\.com\" class="url" rel="contact"/i');
 	}
 	
-	public function getContent($feeditem) {
-		# cut off the username
-		$content = $feeditem->get_content();
-        return substr($content, strpos($content, ': ') + 2);
-	}
-	
     public function getFeedUrl($username) {
 	    return 'http://'.$username.'.jaiku.com/feed/rss';
 	}
