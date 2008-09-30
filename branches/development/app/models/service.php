e67f2dd6d269a6ee45d1a95445234c425315a8ce
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-09-30 05:34:21 +0000

    Fixing a small issue with the cache path for HtmlPurifier

--- branches/development/app/models/service.php
+++ branches/development/app/models/service.php
@@ -436,7 +436,7 @@ class PhotoFilter implements IServiceTypeFilter {
 	
 	public function filter($item) {
 		$config = HTMLPurifier_Config::createDefault();
-		$config->set('Cache', 'SerializerPath', APP . '/tmp/cache/htmlpurifier');
+		$config->set('Cache', 'SerializerPath', CACHE . 'htmlpurifier');
 		$config->set('HTML', 'Allowed', 'img[src|alt]');
 
 		$purifier = new HTMLPurifier($config);
