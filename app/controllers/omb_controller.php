43b2f5bd6d1284c9e81700ed8f6f38d399b8ce65
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-10-11 15:18:23 +0000

    Implementing OmbController::authorize()

--- app/controllers/omb_controller.php
+++ app/controllers/omb_controller.php
@@ -30,7 +30,37 @@ class OmbController extends AppController {
 	}
 	
 	public function authorize() {
-		// TODO add implementation
+		$this->writeToSessionIfParameterIsSet('OAuth.request_token', 'oauth_token');
+		$this->writeToSessionIfParameterIsSet('OAuth.callback_url', 'oauth_callback');
+		
+		if (!$this->Session->check('Identity')) {
+			$this->Session->write('Login.success_url', '/pages/omb/authorize');
+			$this->redirect('/pages/login');
+		}
+		
+		if (empty($this->params['form'])) {
+			if (!$this->Session->check('OAuth.request_token')) {
+				if (isset($this->params['url']['oauth_token'])) {
+					$this->Session->write('OAuth.request_token', $this->params['url']['oauth_token']);
+				} else {
+					$this->render('no_token');
+				}
+			}
+			
+			$this->set('headline', 'Authorize access');
+		} else {
+			if (isset($this->params['form']['allow'])) {
+				$this->OmbRequestToken->authorize($this->Session->read('OAuth.request_token'), $this->Session->read('Identity.id'));
+				$redirectTo = $this->Session->read('OAuth.callback_url');
+			} else {
+				$redirectTo = '/';
+			}
+			
+			$this->Session->delete('OAuth.request_token');
+			$this->Session->delete('OAuth.callback_url');
+			
+			$this->redirect($redirectTo);
+		}
 	}
 	
 	public function post_notice() {
@@ -46,5 +76,11 @@ class OmbController extends AppController {
 		$server->add_signature_method(new OAuthSignatureMethod_HMAC_SHA1());
 		
 		return $server;
+	}
+	
+	private function writeToSessionIfParameterIsSet($sessionKey, $paramKey) {
+		if (isset($this->params['url'][$paramKey])) {
+			$this->Session->write($sessionKey, $this->params['url'][$paramKey]);
+		}
 	}
 }
--- app/models/omb/omb_request_token.php
+++ app/models/omb/omb_request_token.php
@@ -1,5 +1,8 @@
 <?php
 
 class OmbRequestToken extends AppModel {
-	
+
+	public function authorize($token_key, $identity_id) {
+		$this->updateAll(array('authorized' => true, 'identity_id' => $identity_id), array('OmbRequestToken.token_key' => $token_key));
+	}
 }
