eda5ac1277d07f5580f21cc1d1448b3c08e1322b
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-10-11 12:22:14 +0000

    Implementing request token functionality for OMB

--- branches/development/app/controllers/omb_controller.php
+++ branches/development/app/controllers/omb_controller.php
@@ -1,9 +1,28 @@
 <?php
 
+App::import('Vendor', 'oauth', array('file' => 'OAuth'.DS.'OAuth.php'));
+
 class OmbController extends AppController {
+	public $uses = array('OmbDataStore', 'OmbRequestToken');
 	
 	public function request_token() {
-		// TODO add implementation
+		Configure::write('debug', 0);
+		$server = $this->getServer();
+		
+		// to avoid an "invalid signature" error we have to unset this param
+		unset($_GET['url']);
+		
+		try {
+			$request = OAuthRequest::from_request();
+			$this->OmbDataStore->set_omb_listener($request->get_parameter('omb_listener'));
+			$request_token = $server->fetch_request_token($request);
+		  	echo $request_token;
+		} catch (OAuthException $e) {
+			print($e->getMessage() . "\n<hr />\n");
+		  	print_r($request);
+		}
+		
+		exit();
 	}
 	
 	public function access_token() {
@@ -20,5 +39,12 @@ class OmbController extends AppController {
 	
 	public function update_profile() {
 		// TODO add implementation
+	}
+	
+	private function getServer() {
+		$server = new OAuthServer($this->OmbDataStore);
+		$server->add_signature_method(new OAuthSignatureMethod_HMAC_SHA1());
+		
+		return $server;
 	}
 }
---
+++ branches/development/app/models/omb/omb_request_token.php
@@ -0,0 +1,5 @@
+<?php
+
+class OmbRequestToken extends AppModel {
+	
+}
