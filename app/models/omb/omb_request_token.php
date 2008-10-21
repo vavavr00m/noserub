9897eaf99635db6f6b41ca0a6b2f1ccbf7d8662a
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-10-21 06:01:05 +0000

    Adding deleteExpired() to OmbRequestToken

--- app/models/omb/omb_request_token.php
+++ app/models/omb/omb_request_token.php
@@ -5,4 +5,8 @@ class OmbRequestToken extends AppModel {
 	public function authorize($token_key, $identity_id) {
 		$this->updateAll(array('authorized' => true, 'identity_id' => $identity_id), array('OmbRequestToken.token_key' => $token_key));
 	}
+	
+	public function deleteExpired() {
+		$this->deleteAll(array('OmbRequestToken.modified <= DATE_SUB(NOW(), INTERVAL 24 HOUR)'));
+	}
 }
