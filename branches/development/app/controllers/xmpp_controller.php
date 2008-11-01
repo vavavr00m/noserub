92e793de3fcd4692a7c8649e1dca444259af1aa4
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-11-01 14:17:30 +0000

    Fixing ugly indentations

--- branches/development/app/controllers/xmpp_controller.php
+++ branches/development/app/controllers/xmpp_controller.php
@@ -27,40 +27,40 @@ class XmppController extends AppController {
             while(!$conn->isDisconnected()) {
                 $payloads = $conn->processUntil(array('message', 'presence', 'end_stream', 'session_start'));
                 foreach($payloads as $event) {
-                        $pl = $event[1];
-                        switch($event[0]) {
-                                case 'message': 
-        if($pl['body']=='') {
-          #  print $pl['from'] . ' is typing...' . "\n";
-            break;
-        }
-        if($pl['body'] == 'uptime') {
-            $conn->message($pl['from'], uptime(), $pl['type']);
-            break;
-        }
-                                        print "---------------------------------------------------------------------------------\n";
-                                        print "Message from: {$pl['from']}\n";
-                                        if(isset($pl['Subject']) && $pl['subject']) print "Subject: {$pl['subject']}\n";
-                                        print $pl['body'] . "\n";
-                                        print "---------------------------------------------------------------------------------\n";
-                                        $conn->message($pl['from'], $body="Thanks for sending me \"{$pl['body']}\".", $type=$pl['type']);
-                                        if($pl['body'] == 'quit') $conn->disconnect();
-                                        if($pl['body'] == 'break') $conn->send("</end>");
-                                break;
-                                case 'presence':
-                                        print "Presence: {$pl['from']} [{$pl['show']}] {$pl['status']}\n";
-                                break;
-                                case 'session_start':
-                                    print "Session Start\n";
-                                        $conn->getRoster();
-                                        $conn->presence($status="Cheese!");
-                                break;
-                        }
+                	$pl = $event[1];
+                    
+                	switch($event[0]) {
+                    	case 'message': 
+					        if($pl['body']=='') {
+					          #  print $pl['from'] . ' is typing...' . "\n";
+					            break;
+					        }
+					        if($pl['body'] == 'uptime') {
+					            $conn->message($pl['from'], uptime(), $pl['type']);
+					            break;
+					        }
+                            print "---------------------------------------------------------------------------------\n";
+                            print "Message from: {$pl['from']}\n";
+                            if(isset($pl['Subject']) && $pl['subject']) print "Subject: {$pl['subject']}\n";
+                            print $pl['body'] . "\n";
+                            print "---------------------------------------------------------------------------------\n";
+                            $conn->message($pl['from'], $body="Thanks for sending me \"{$pl['body']}\".", $type=$pl['type']);
+                            if($pl['body'] == 'quit') $conn->disconnect();
+                            if($pl['body'] == 'break') $conn->send("</end>");
+                            break;
+                        case 'presence':
+                            print "Presence: {$pl['from']} [{$pl['show']}] {$pl['status']}\n";
+                            break;
+                        case 'session_start':
+                            print "Session Start\n";
+                            $conn->getRoster();
+                            $conn->presence($status="Cheese!");
+                            break;
+                    }
                 }
             }
         } catch(XMPPHP_Exception $e) {
             die($e->getMessage());
         }
-        
     }
 }
