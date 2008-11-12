ac578a00e03ccfd0681596217b377d47a781333a
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-11-12 09:53:55 +0000

    Refactoring: extracting classes from admin model

---
+++ app/models/admin/extensions_checker.php
@@ -0,0 +1,22 @@
+<?php
+
+class ExtensionsChecker {
+	
+	public static function check() {
+		$result = array();
+    	
+        if (!extension_loaded('curl')) {
+        	$result = am($result, array('curl' => __('needed for communicating with other servers', true)));
+        }
+        
+    	if (!extension_loaded('gd')) {
+        	$result = am($result, array('GD' => __('needed for image handling', true))); 
+        }
+    	
+        if (!(function_exists('gmp_init') || function_exists('bcscale'))) {
+        	$result = am($result, array(__('GMP or BCMath', true) => __('needed for OpenID functionality', true)));
+        }
+        
+        return $result;
+	}
+}
