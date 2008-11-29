5a41c0bc772c687f372d6b2d4d8f533622a16472
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-11-29 06:39:04 +0000

    Adding helper method UrlUtil::addHttpIfNoProtocolSpecified()

--- branches/development/app/vendors/url_util.php
+++ branches/development/app/vendors/url_util.php
@@ -2,6 +2,14 @@
 
 class UrlUtil {
 	
+	public static function addHttpIfNoProtocolSpecified($url) {
+		if (!UrlUtil::startsWithHttpOrHttps($url) && trim($url) != '') {
+			$url = 'http://' . $url;
+		}
+		
+		return $url;
+	}
+	
 	public static function startsWithHttpOrHttps($url) {
     	if (strpos($url, 'http://') === 0 || strpos($url, 'https://') === 0) {
     		return true;
