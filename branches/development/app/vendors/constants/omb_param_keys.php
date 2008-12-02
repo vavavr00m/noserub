6c85cb34efa4c8da89c5f4a4558dd557caacfb73
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-12-02 10:28:59 +0000

    Adding new class with constants

---
+++ branches/development/app/vendors/constants/omb_param_keys.php
@@ -0,0 +1,15 @@
+<?php
+
+class OmbParamKeys {
+	const LISTENEE = 'omb_listenee';
+	const LISTENEE_AVATAR = 'omb_listenee_avatar';
+	const LISTENEE_BIO = 'omb_listenee_bio';
+	const LISTENEE_FULLNAME = 'omb_listenee_fullname';
+	const LISTENEE_HOMEPAGE = 'omb_listenee_homepage';
+	const LISTENEE_LICENSE = 'omb_listenee_license';
+	const LISTENEE_LOCATION = 'omb_listenee_location';
+	const LISTENEE_NICKNAME = 'omb_listenee_nickname';
+	const LISTENEE_PROFILE = 'omb_listenee_profile';
+	const LISTENER = 'omb_listener';
+	const VERSION = 'omb_version';
+}
