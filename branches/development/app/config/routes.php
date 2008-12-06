41dd7f5ae14fb4f2781b0d342da391104c00f271
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2008-12-06 10:51:06 +0000

    Removing invalid copyright headers

--- branches/development/app/config/routes.php
+++ branches/development/app/config/routes.php
@@ -1,33 +1,4 @@
 <?php
-/* SVN FILE: $Id: routes.php 4410 2007-02-02 13:31:21Z phpnut $ */
-/**
- * Short description for file.
- *
- * In this file, you set up routes to your controllers and their actions.
- * Routes are very important mechanism that allows you to freely connect
- * different urls to chosen controllers and their actions (functions).
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) :  Rapid Development Framework <http://www.cakephp.org/>
- * Copyright 2005-2007, Cake Software Foundation, Inc.
- *								1785 E. Sahara Avenue, Suite 490-204
- *								Las Vegas, Nevada 89104
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @filesource
- * @copyright		Copyright 2005-2007, Cake Software Foundation, Inc.
- * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package			cake
- * @subpackage		cake.app.config
- * @since			CakePHP(tm) v 0.2.9
- * @version			$Revision$
- * @modifiedby		$LastChangedBy: phpnut $
- * @lastmodified	$Date$
- * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
- */
 
 Router::connect('/', array('controller' => 'contacts', 'action' => 'network'));
 Router::connect('/social_stream/:filter/:output/', array('controller' => 'identities', 'action' => 'social_stream'));
