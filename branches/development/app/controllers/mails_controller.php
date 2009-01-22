ac4a85469c9ebfac88a58eb1e895ecc289f3343c
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-01-22 16:45:53 +0000

    making sure, that the mail sending route may only be called thorugh requestAction

--- branches/development/app/controllers/mails_controller.php
+++ branches/development/app/controllers/mails_controller.php
@@ -5,6 +5,13 @@ class MailsController extends AppController {
     public $components = array('Email');
     
     public function send() {
+        if(!isset($this->params['bare']) || 
+           !$this->params['bare']) {
+            # check that this can only be accessed through
+            # requestAction
+            return false;
+        }
+        
         $this->Email->to       = $this->params['to'];
         $this->Email->subject  = $this->params['subject'];
         $this->Email->from     = $this->params['from'];
