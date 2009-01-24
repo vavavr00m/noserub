fa084090a663dffdbbec0fda41e5d91640449dfb
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-01-24 14:13:54 +0000

    Ensuring the required URLs are present when creating an OmbLocalServiceDefinition

--- branches/development/app/tests/cases/vendors/omb/omb_local_service_definition.test.php
+++ branches/development/app/tests/cases/vendors/omb/omb_local_service_definition.test.php
@@ -9,15 +9,16 @@ class OmbLocalServiceDefinitionTest extends CakeTestCase {
 	private $postNoticeUrl = 'http://example.com/postNotice';
 	private $updateProfileUrl = 'http://example.com/updateProfile';
 	private $serviceDefinition = null;
+	private $urls = null;
 	
 	public function setUp() {
-		$urls = array(OauthConstants::REQUEST => $this->requestTokenUrl,
-					  OauthConstants::AUTHORIZE => $this->authorizeUrl,
-					  OauthConstants::ACCESS => $this->accessTokenUrl,
-					  OmbConstants::POST_NOTICE => $this->postNoticeUrl,
-					  OmbConstants::UPDATE_PROFILE => $this->updateProfileUrl);
+		$this->urls = array(OauthConstants::REQUEST => $this->requestTokenUrl,
+					  		OauthConstants::AUTHORIZE => $this->authorizeUrl,
+					  		OauthConstants::ACCESS => $this->accessTokenUrl,
+					  		OmbConstants::POST_NOTICE => $this->postNoticeUrl,
+					  		OmbConstants::UPDATE_PROFILE => $this->updateProfileUrl);
 					  		
-		$this->serviceDefinition = new OmbLocalServiceDefinition($this->localId, $urls);
+		$this->serviceDefinition = new OmbLocalServiceDefinition($this->localId, $this->urls);
 	}
 	
 	public function testOmbLocalServiceDefinition() {
@@ -27,5 +28,64 @@ class OmbLocalServiceDefinitionTest extends CakeTestCase {
 		$this->assertEqual($this->accessTokenUrl, $this->serviceDefinition->getAccessTokenUrl());
 		$this->assertEqual($this->postNoticeUrl, $this->serviceDefinition->getPostNoticeUrl());
 		$this->assertEqual($this->updateProfileUrl, $this->serviceDefinition->getUpdateProfileUrl());
+	}
+	
+	public function testOmbLocalServiceDefinitionWithMissingAccessTokenUrl() {
+		unset($this->urls[OauthConstants::ACCESS]);
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithMissingAuthorizeUrl() {
+		unset($this->urls[OauthConstants::AUTHORIZE]);
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithMissingPostNoticeUrl() {
+		unset($this->urls[OmbConstants::POST_NOTICE]);
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithMissingRequestTokenUrl() {
+		unset($this->urls[OauthConstants::REQUEST]);
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithMissingUpdateProfileUrl() {
+		unset($this->urls[OmbConstants::UPDATE_PROFILE]);
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithInvalidAccessTokenUrl() {
+		$this->urls[OauthConstants::ACCESS] = 'invalid';
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithInvalidAuthorizeUrl() {
+		$this->urls[OauthConstants::AUTHORIZE] = 'invalid';
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithInvalidPostNoticeUrl() {
+		$this->urls[OmbConstants::POST_NOTICE] = 'invalid';
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithInvalidRequestTokenUrl() {
+		$this->urls[OauthConstants::REQUEST] = 'invalid';
+		$this->assertFailingConstructor();
+	}
+	
+	public function testOmbLocalServiceDefinitionWithInvalidUpdateProfileUrl() {
+		$this->urls[OmbConstants::UPDATE_PROFILE] = 'invalid';
+		$this->assertFailingConstructor();
+	}
+	
+	private function assertFailingConstructor() {
+		try {
+			new OmbLocalServiceDefinition($this->localId, $this->urls);
+			$this->fail('Exception expected');
+		} catch (Exception $e) {
+			$this->assertTrue(true);
+		}
 	}
 }
