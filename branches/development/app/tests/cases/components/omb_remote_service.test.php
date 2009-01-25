fb73cb49dd4a41bf719f392c424386eb36912b76
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-01-25 08:01:40 +0000

    Moving class OmbListeneeBio to its own file

--- branches/development/app/tests/cases/components/omb_remote_service.test.php
+++ branches/development/app/tests/cases/components/omb_remote_service.test.php
@@ -222,21 +222,6 @@ class OmbUpdatedProfileDataTest extends CakeTestCase {
 	}
 }
 
-class OmbListeneeBioTest extends CakeTestCase {
-	public function testConstruct() {
-		$bio = 'My bio';
-		$listeneeBio = new OmbListeneeBio($bio);
-		$this->assertEqual(OmbParamKeys::LISTENEE_BIO, $listeneeBio->getKey());
-		$this->assertEqual($bio, $listeneeBio->getValue());
-	}
-	
-	public function testConstructWithTooLongBio() {
-		$bio = str_repeat('a', OmbListeneeBio::MAX_LENGTH + 1);
-		$listeneeBio = new OmbListeneeBio($bio);
-		$this->assertEqual(OmbListeneeBio::MAX_LENGTH, strlen($listeneeBio->getValue()));
-	}
-}
-
 class OmbListeneeFullnameTest extends CakeTestCase {
 	public function testConstruct() {
 		$fullname = 'Firstname Lastname';
