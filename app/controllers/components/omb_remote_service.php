db7457d3577bdc3dc0090b046db7aeb800da746b
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-01-25 08:33:13 +0000

    Moving class OmbListeneeLocation to its own file

--- app/controllers/components/omb_remote_service.php
+++ app/controllers/components/omb_remote_service.php
@@ -11,7 +11,8 @@ if (!class_exists('Auth_Yadis_Yadis')) {
 }
 App::import('Vendor', 'oauth', array('file' => 'OAuth'.DS.'OAuth.php'));
 App::import('Vendor', array('OauthConstants', 'OmbConstants', 'OmbListeneeAvatar', 
-							'OmbListeneeBio', 'OmbListeneeFullname', 'OmbParam', 'OmbParamKeys'));
+							'OmbListeneeBio', 'OmbListeneeFullname', 'OmbListeneeLocation',
+							'OmbParam', 'OmbParamKeys'));
 
 class OmbRemoteServiceComponent extends Object {
 	public $components = array('OmbOauthConsumer', 'Session');
@@ -294,22 +295,6 @@ class OmbUpdatedProfileData {
 		}
 		
 		return $result;
-	}
-}
-
-class OmbListeneeLocation extends OmbParam {
-	const MAX_LENGTH = 254; // spec says "less than 255 chars"
-	
-	public function __construct($location) {
-		parent::__construct($this->shortenIfTooLong($location));
-	}
-	
-	public function getKey() {
-		return OmbParamKeys::LISTENEE_LOCATION;
-	}
-	
-	private function shortenIfTooLong($location) {
-		return substr($location, 0, self::MAX_LENGTH);
 	}
 }
 
