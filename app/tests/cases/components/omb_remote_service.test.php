323217cb4af69ed0e59564142041e8413a111652
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-01-25 07:53:42 +0000

    Moving class OmbListeneeAvatar to its own file

--- app/tests/cases/components/omb_remote_service.test.php
+++ app/tests/cases/components/omb_remote_service.test.php
@@ -222,29 +222,6 @@ class OmbUpdatedProfileDataTest extends CakeTestCase {
 	}
 }
 
-class OmbListeneeAvatarTest extends CakeTestCase {
-	public function testConstruct() {
-		$avatarName = 'myavatar';
-		$avatarUrl = Configure::read('NoseRub.full_base_url').'static/avatars/'.$avatarName.'-medium.jpg';
-		$listeneeAvatar = new OmbListeneeAvatar($avatarName);
-		$this->assertEqual(OmbParamKeys::LISTENEE_AVATAR, $listeneeAvatar->getKey());
-		$this->assertEqual($avatarUrl, $listeneeAvatar->getValue());
-	}
-	
-	public function testConstructWithEmptyAvatar() {
-		$avatarName = '';
-		$listeneeAvatar = new OmbListeneeAvatar($avatarName);
-		$this->assertEqual($avatarName, $listeneeAvatar->getValue());
-	}
-	
-	public function testConstructWithGravatar() {
-		$gravatar = 'http://gravatar.com/avatar/xy';
-		$gravatar96x96 = $gravatar . '?s=96';
-		$listeneeAvatar = new OmbListeneeAvatar($gravatar);
-		$this->assertEqual($gravatar96x96, $listeneeAvatar->getValue());
-	}
-}
-
 class OmbListeneeBioTest extends CakeTestCase {
 	public function testConstruct() {
 		$bio = 'My bio';
