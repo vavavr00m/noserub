9d19ea2a339edf8ca6be1f5ee4a950e7c3cb8167
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-01-28 13:23:21 +0000

    Bugfix: removing dots in nickname because laconica doesn't like them

--- branches/development/app/vendors/omb/params/omb_listenee_nickname.php
+++ branches/development/app/vendors/omb/params/omb_listenee_nickname.php
@@ -2,7 +2,15 @@
 App::import('Vendor', array('OmbParam', 'OmbParamKeys'));
 
 class OmbListeneeNickname extends OmbParam {
+	public function __construct($nickname) {
+		parent::__construct($this->removeDots($nickname));
+	}
+	
 	public function getKey() {
 		return OmbParamKeys::LISTENEE_NICKNAME;
+	}
+	
+	private function removeDots($nickname) {
+		return str_replace('.', '', $nickname);
 	}
 }
