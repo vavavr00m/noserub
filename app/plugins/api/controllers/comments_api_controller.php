85e5149db58e905a55e7f96a33120508f6f48426
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-02-15 08:01:15 +0000

    Moving comments API to the API plugin

---
+++ app/plugins/api/controllers/comments_api_controller.php
@@ -0,0 +1,52 @@
+<?php
+
+class CommentsApiController extends ApiAppController {
+	public $uses = array('Comment');
+	public $components = array('Api');
+	
+	/**
+	 * return the last 100 comments
+	 *
+	 * todo: make this configurable by date (eg. get all comments since 2008-12-01 12:34:29)
+	 */
+	public function get_comments() {
+		$this->Comment->contain('Entry', 'Identity');
+		$comments = $this->Comment->find(
+			'all',
+			array(
+				'order'  => 'Comment.published_on DESC',
+				'limit'  => 100
+			)
+		);
+
+		$this->set('data', $this->cleanUpData($comments));
+		$this->Api->render();
+	}
+	
+	private function cleanUpData(array $comments) {
+		$data = array();
+		foreach($comments as $comment) {
+			if($comment['Entry']['url']) {
+				$url = $comment['Entry']['url'];
+			} else {
+				$url = Router::url('/entry/' . $comment['Entry']['id'], true);
+			}
+			
+			if($comment['Entry']['uid']) {
+				$uid = $comment['Entry']['uid'];
+			} else {
+				$uid = md5($url);
+			}
+			
+			$data[] = array(
+				'uid'          => $uid,
+				'url'          => $url,
+				'commented_by' => $comment['Identity']['username'],
+				'commented_on' => $comment['Comment']['published_on'],
+				'comment'      => $comment['Comment']['content']
+			);
+		}
+		
+		return $data;
+	}
+}
