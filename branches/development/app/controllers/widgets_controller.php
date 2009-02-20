329464ab9c007eee9eebecd9e0d1ec274fd32e91
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-02-20 23:52:34 +0000

    some more work on widgets and themes

--- branches/development/app/controllers/widgets_controller.php
+++ branches/development/app/controllers/widgets_controller.php
@@ -15,7 +15,7 @@ class WidgetsController extends AppController {
  	 */
  	 
  	public function contact_filter() {
- 	    if($this->context['logged_in_identity']) {
+ 	    if($this->context['is_self']) {
  	        $this->dynamicUse('Contact');
  	    
  	        $this->set('contact_tags', $this->Contact->getTagList($this->context['logged_in_identity']['id']));
@@ -36,71 +36,69 @@ class WidgetsController extends AppController {
 	/**
 	 * Contacts
 	 */
-	 /**
-      * contacts of a given identity.
-      */
-     public function contacts_for_identity() {
-         $this->dynamicUse('Contact');
+
+    /**
+     * contacts of a given identity.
+     */
+    public function contacts_for_identity() {
+        $this->dynamicUse('Contact');
          
-         $session_identity = $this->Session->read('Identity');
-
-         $identity_id = $this->params['identity_id'];
-         $this->Contact->Identity->contain();
-         $this->Contact->Identity->id = $identity_id;
-         $this->set('identity', $this->Contact->Identity->read());
-
-         $tag_filter = $this->Session->read('Filter.Contact.Tag');
-         if(!$tag_filter) {
-             $tag_filter = 'all';
-         }
-
-         $is_self = $session_identity && $session_identity['id'] == $identity_id;
-         # get (filtered) contacts
-         if($is_self) {
-             # this is my network, so I can show every contact
-             $contact_filter = array('tag' => $tag_filter);
-         } else {
-             # this is someone elses network, so I show only the noserub contacts
-             $contact_filter = array('tag' => $tag_filter, 'type' => 'public');
-         }
-         $data = $this->Contact->getForDisplay($identity_id, $contact_filter, 9);
-         $contacts = array();
-         foreach($data as $key => $value) {
-             $contacts[] = $value['WithIdentity'];
-         }
-         $this->set('data', $contacts);
+        $identity_id = $this->getIdentityId();
+        $this->Contact->Identity->contain();
+        $this->Contact->Identity->id = $identity_id;
+        $this->set('identity', $this->Contact->Identity->read());
+        
+        $tag_filter = $this->Session->read('Filter.Contact.Tag');
+        if(!$tag_filter) {
+            $tag_filter = 'all';
+        }
+        
+        # get (filtered) contacts
+        if($this->context['is_self']) {
+            # this is my network, so I can show every contact
+            $contact_filter = array('tag' => $tag_filter);
+        } else {
+            # this is someone elses network, so I show only the noserub contacts
+            $contact_filter = array('tag' => $tag_filter, 'type' => 'public');
+        }
+        $data = $this->Contact->getForDisplay($identity_id, $contact_filter, 9);
+        $contacts = array();
+        foreach($data as $key => $value) {
+            $contacts[] = $value['WithIdentity'];
+        }
+        $this->set('data', $contacts);
      }
 
      public function my_contacts() {
-         $this->dynamicUse('Contact');
-         
-         $logged_in_identity_id = $this->Session->read('Identity.id');
-         if(!$logged_in_identity_id) {
-             return false;
-         }
-
-         # get contacts of the displayed profile
-         $all_contacts = $this->Contact->getForIdentity($logged_in_identity_id);
-
-         $contacts = array();
-         $num_private_contacts = 0;
-         $num_noserub_contacts = 0;
-         foreach($all_contacts as $contact) {
-             if(strpos($contact['WithIdentity']['username'], '@') === false) {
-                 $num_noserub_contacts++;
-                 if(count($contacts) < 9) {
-                     $contacts[] = $contact;
-                 }
-             } else {
-                 $num_private_contacts++;
-                 if(count($contacts) < 9) {
-                     $contacts[] = $contact;
-                 }
-             }
-         }
-         $this->set('num_private_contacts', $num_private_contacts);
-         $this->set('num_noserub_contacts', $num_noserub_contacts);
-         $this->set('data', $contacts);
+        $this->dynamicUse('Contact');
+        
+        $logged_in_identity_id = $this->Session->read('Identity.id');
+        if(!$logged_in_identity_id) {
+            return false;
+        }
+        
+        # get contacts of the displayed profile
+        $all_contacts = $this->Contact->getForIdentity($logged_in_identity_id);
+        
+        $contacts = array();
+        $num_private_contacts = 0;
+        $num_noserub_contacts = 0;
+        foreach($all_contacts as $contact) {
+            if(strpos($contact['WithIdentity']['username'], '@') === false) {
+                $num_noserub_contacts++;
+                if(count($contacts) < 9) {
+                    $contacts[] = $contact;
+                }
+            } else {
+                $num_private_contacts++;
+                if(count($contacts) < 9) {
+                    $contacts[] = $contact;
+                }
+            }
+        }
+        $this->set('num_private_contacts', $num_private_contacts);
+        $this->set('num_noserub_contacts', $num_noserub_contacts);
+        $this->set('data', $contacts);
      }
      
      /**
@@ -119,11 +117,34 @@ class WidgetsController extends AppController {
              $models = array($models);
          }
 
-         foreach($models as $model) {
+        foreach($models as $model) {
              if(!isset($this->{$model})) {
                  App::import('Model', $model);
                  eval ("\$this->{$model} = new {$model}();");
              }
          }
+     }
+     
+    /**
+     * If no identity_id was given by param, we're looking
+     * for the identity we currently look at. if none
+     * is found, the logged in identity is used
+     *
+     * @return $int
+     */
+    private function getIdentityId() {
+        $identity_id = isset($this->params['identity_id']) ? $this->params['identity_id'] : 0;
+        if(!$identity_id) {
+            # if no identity_id was given in the params, use the one
+            # from the current page
+            if($this->context['identity']) {
+                $identity_id = $this->context['identity']['id'];
+            } else if($this->context['logged_in_identity']) {
+                # if not, use the logged in identity
+                $identity_id = $this->context['logged_in_identity']['id'];
+            }
+         }
+     
+        return $identity_id;
      }
 }
