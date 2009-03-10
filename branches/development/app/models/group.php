166292c02e633f628c5f0f30c7c805d1d7737587
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-03-10 22:56:17 +0000

    removing relations for now, as they are somewhat wrong and are not needed for the upcoming 0.8.2

--- branches/development/app/models/group.php
+++ branches/development/app/models/group.php
@@ -4,18 +4,18 @@
 class Group extends AppModel {
     public $belongsTo = array('Network');
     
-    public $hasAndBelongsToMany = array(
-            'GroupSubscriber' => array(
-                'className'  => 'Identity',
-                'joinTable'  => 'group_subscriptions',
-                'foreignKey' => 'group_id',
-                'associationForeignKey' => 'identity_id'
-            ),
-            'GroupAdmin' => array(
-                'className' => 'Identity',
-                'joinTable' => 'group_admins',
-                'foreignKey' => 'group_id',
-                'associationForeignKey' => 'identity_id' 
-            )
-    );
+    // public $hasAndBelongsToMany = array(
+    //             'GroupSubscriber' => array(
+    //                 'className'  => 'Identity',
+    //                 'joinTable'  => 'group_subscriptions',
+    //                 'foreignKey' => 'group_id',
+    //                 'associationForeignKey' => 'identity_id'
+    //             ),
+    //             'GroupAdmin' => array(
+    //                 'className' => 'Identity',
+    //                 'joinTable' => 'group_admins',
+    //                 'foreignKey' => 'group_id',
+    //                 'associationForeignKey' => 'identity_id' 
+    //             )
+    //     );
 }
