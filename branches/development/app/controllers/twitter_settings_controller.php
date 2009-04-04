085ec56e689de1a37b38a3ecef6d3c5f4ff15923
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-04-04 09:09:31 +0000

    Adding handling for when the Twitter feature is not enabled

--- branches/development/app/controllers/twitter_settings_controller.php
+++ branches/development/app/controllers/twitter_settings_controller.php
@@ -1,7 +1,17 @@
 <?php
 
 class TwitterSettingsController extends AppController {
+	public $components = array('RequestHandler');
+
 	public function index() {
+		if ($this->RequestHandler->isGet()) {
+			$this->set('isTwitterFeatureEnabled', $this->isTwitterFeatureEnabled());
+		}
 		// TODO implement this method
+	}
+
+	private function isTwitterFeatureEnabled() {
+		return (Configure::read('context.network.twitter_consumer_key') != '' &&
+				Configure::read('context.network.twitter_consumer_secret') != '');
 	}
 }
--- branches/development/app/views/themed/default/twitter_settings/index.ctp
+++ branches/development/app/views/themed/default/twitter_settings/index.ctp
@@ -1,5 +1,9 @@
 <div id="inhalt">
 	<div id="left">
+		<?php if ($isTwitterFeatureEnabled): ?>
+		<?php else: ?>
+			Twitter feature is not enabled in your network.
+		<?php endif; ?>
 	</div>
 </div>
 <div id="rechts">
