7b4c9f2f2b40b9cbb49c1f6ad9e47e9f2519c72a
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-04-11 07:46:29 +0000

    Removing useless comments from configuration files

--- branches/development/app/config/core.php
+++ branches/development/app/config/core.php
@@ -1,31 +1,4 @@
 <?php
-/* SVN FILE: $Id: core.php 7089 2008-06-02 17:35:56Z gwoo $ */
-/**
- * This is core configuration file.
- *
- * Use it to configure core behavior of Cake.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) :  Rapid Development Framework <http://www.cakephp.org/>
- * Copyright 2005-2008, Cake Software Foundation, Inc.
- *								1785 E. Sahara Avenue, Suite 490-204
- *								Las Vegas, Nevada 89104
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @filesource
- * @copyright		Copyright 2005-2008, Cake Software Foundation, Inc.
- * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package			cake
- * @subpackage		cake.app.config
- * @since			CakePHP(tm) v 0.2.9
- * @version			$Revision$
- * @modifiedby		$LastChangedBy: gwoo $
- * @lastmodified	$Date$
- * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
- */
 /**
  * CakePHP Debug Level:
  *
