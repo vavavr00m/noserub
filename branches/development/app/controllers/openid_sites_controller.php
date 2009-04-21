4eeb79ea69b21d0365c33ace47ff60cdf888eba5
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-04-21 14:45:43 +0000

    Fixing same bug as in r1566, this time for the OpenID settings page

--- branches/development/app/controllers/openid_sites_controller.php
+++ branches/development/app/controllers/openid_sites_controller.php
@@ -3,36 +3,23 @@
 class OpenidSitesController extends AppController {
 	public $uses = array('OpenidSite');
 	
+	public function beforeFilter() {
+		parent::beforeFilter();
+		
+		if (!Context::isLoggedInIdentity()) {
+			$this->redirect('/');
+		}
+	}
+	
 	public function index() {
-	    $this->checkSecure();
-	    
-	    $this->loadModel('Identity');
-        $username = isset($this->params['username']) ? $this->params['username'] : '';
-        $splitted = $this->Identity->splitUsername($username);
-        $session_identity = $this->Session->read('Identity');
-        if(!$session_identity) {
-            # only a logged in user will see this
-            $this->redirect('/');
-        }
-        
-        # get identity that is displayed
-        $this->Identity->contain();
-        $identity = $this->Identity->findByUsername($splitted['username']);
-        if(!$identity || 
-           $identity['Identity']['id'] != $session_identity['id']) {
-            # this identity is not here, or it is not the logged
-            # in identity
-            $this->redirect('/');
-        }
-        
 		$this->set('headline', __('OpenID settings', true));
 		
 		if (!empty($this->data)) {
-			$this->OpenidSite->updateAllAllowedStates($this->data['OpenidSite'], $session_identity['id']);
+			$this->OpenidSite->updateAllAllowedStates($this->data['OpenidSite'], Context::loggedInIdentityId());
 		}
 
 		$this->OpenidSite->contain();
-		$openidSites = $this->OpenidSite->findAllByIdentityId($session_identity['id']);
+		$openidSites = $this->OpenidSite->findAllByIdentityId(Context::loggedInIdentityId());
 		
 		if (!empty($openidSites)) {
 			$this->set('openidSites', $openidSites);
