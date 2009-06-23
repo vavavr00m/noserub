443fa8d6f7f47910d15e055dd28bec9f6810113d
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-06-23 15:54:01 +0000

    fixed welcome message after login; added animation to flash messages

--- branches/development/app/webroot/js/noserub.js
+++ branches/development/app/webroot/js/noserub.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
+    $('div#message').animate({'opacity' : 0.25}, 'medium').animate({'opacity' : 1}, 'medium').animate({'opacity' : 0.5}, 'medium').animate({'opacity' : 1}, 'medium');
+    
     noserub_theme_run();
 });
