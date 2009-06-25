1388e1aa96a6052e2a1634e7d8a34b5a90e53a83
judocoach <judocoach@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-06-25 20:07:42 +0000

    Adding readernaut as a service as per issue #285

---
+++ app/config/sql/migrations/0000148_adding_readernaut_service.sql
@@ -0,0 +1,1 @@
+INSERT INTO  `services` (`id` ,`internal_name` ,`name` ,`url` ,`service_type_id` ,`help` ,`icon` ,`has_feed` ,`is_contact` ,`minutes_between_updates` ,`created` ,`modified`) VALUES (86 ,  'Readernaut',  'Readernaut',  'http://readernaut.com',  '5',  '',  'readernaut.gif',  '1',  '0',  '30', NOW( ) , NOW( ));
