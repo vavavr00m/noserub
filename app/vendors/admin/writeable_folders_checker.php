807b98ec16be2d1039cc57fb6c42029eba4d27a5
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-07-01 05:44:17 +0000

    Checking whether the "photos" folder is writable plus adding a corresponding hint to the installation instructions

--- app/vendors/admin/writeable_folders_checker.php
+++ app/vendors/admin/writeable_folders_checker.php
@@ -3,7 +3,7 @@
 class WriteableFoldersChecker {
 	
 	public static function check() {
-    	$writeableDirectories = array(APP.'tmp', WWW_ROOT.'static'.DS.'avatars');
+    	$writeableDirectories = array(APP.'tmp', WWW_ROOT.'static'.DS.'avatars', WWW_ROOT.'static'.DS.'photos');
     	
         $out = array();
         foreach($writeableDirectories as $directory) {
