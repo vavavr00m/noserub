ce2d1f9e1b6f34f5f044e94583ad50a5636038c7
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-07-02 07:40:58 +0000

    Adding default templates for XML and Json, removing duplicate views

--- branches/development/app/plugins/api/api_app_controller.php
+++ branches/development/app/plugins/api/api_app_controller.php
@@ -2,4 +2,13 @@
 
 class ApiAppController extends AppController {
 	public $components = array('Api');
+	
+	public function beforeRender() {
+		$pathToViewFile = dirname(__FILE__).DS.'views'.DS.$this->viewPath.DS.$this->action.'.ctp';
+		
+		if (!file_exists($pathToViewFile)) {
+			$this->viewPath = $this->layoutPath;
+			$this->action = 'default';
+		}
+	}
 }
