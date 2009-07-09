75a985c402c7daebc331af034e87cd8bf7109fe0
dirk.olbertz <dirk.olbertz@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-07-09 20:39:09 +0000

    using w3c geolocation api to save current location to javascript context

--- branches/development/app/webroot/js/noserub.js
+++ branches/development/app/webroot/js/noserub.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     $('div#message').animate({'opacity' : 0.25}, 'medium').animate({'opacity' : 1}, 'medium').animate({'opacity' : 0.5}, 'medium').animate({'opacity' : 1}, 'medium');
     
+    if(typeof navigator.geolocation != "undefined") {
+        navigator.geolocation.getCurrentPosition(function(data){noserub_context.coords = {latitude: data.coords.latitude, longitude: data.coords.longitude};});
+    } else {
+        noserub_context.coords = {latitude: 0, longitude: 0};
+    }
+
     noserub_theme_start(noserub_context);
     
     if(typeof noserub_start_webcam != "undefined") {
