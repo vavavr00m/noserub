f0c134eaecc05ef746471a69db69adb7a2144b38
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-07-19 14:14:31 +0000

    Making the OauthServiceProviderComponent more flexible by removing the restriction of only handling GET requests

--- branches/development/app/controllers/components/oauth_service_provider.php
+++ branches/development/app/controllers/components/oauth_service_provider.php
@@ -1,14 +1,14 @@
 <?php
+App::import('Vendor', 'oauth', array('file' => 'Oauth'.DS.'OAuth.php'));
 
 class OauthServiceProviderComponent extends Object {
+	
 	public function getAccessTokenKeyOrDie() {
-		App::import('Vendor', 'oauth', array('file' => 'Oauth'.DS.'OAuth.php'));
-		
 		$server = $this->getServer();
+		$this->removeUrlParamFromQueryString();
 		
 		try {
-			unset($_GET['url']);
-			$request = OAuthRequest::from_request('GET');
+			$request = OAuthRequest::from_request();
 			$server->verify_request($request);
 		} catch (OAuthException $e) {
 			header('HTTP/1.1 403 Forbidden');
@@ -25,5 +25,17 @@ class OauthServiceProviderComponent extends Object {
 		$server->add_signature_method(new OAuthSignatureMethod_HMAC_SHA1());
 		
 		return $server;
+	}
+	
+	private function removeUrlParamFromQueryString() {
+		$queryString = $_SERVER['QUERY_STRING'];
+		
+		if (strpos($queryString, '&') === false) {
+			$queryString = '';
+		} else {
+			$queryString = substr($queryString, strpos($queryString, '&') + 1);
+		}
+		
+		$_SERVER['QUERY_STRING'] = $queryString;
 	}
 }
