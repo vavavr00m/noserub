d3706bc7140e57e6a24c6ff20a7ed4b1eec13b05
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-07-25 13:41:15 +0000

    Using OAuth when sending status to Twitter, removing Twitter usernames/passwords from database

--- app/models/twitter_account.php
+++ app/models/twitter_account.php
@@ -30,79 +30,36 @@ class TwitterAccount extends AppModel {
 		}
 	}
 	
-	/**
-	 * many thanks to laconi.ca for this code!
-     * can be found in lib/util.php there.
-	 */
 	public function updateStatus($identity_id, $text) {
-		if (Configure::read('NoseRub.allow_twitter_bridge') === false) {
+		if (Context::isTwitterFeatureEnabled() === false) {
             return false;       
         }
 
         $this->contain();
         $data = $this->findByIdentityId($identity_id);
-        if($data['TwitterAccount']['bridge_active'] != 1) {
-            return false;
-        }
-             
-    	$twitter_username = $data['TwitterAccount']['username'];
-    	$twitter_password = $data['TwitterAccount']['password'];
-    	$uri = 'http://www.twitter.com/statuses/update.json';
-
-    	$options = array(
-    		CURLOPT_USERPWD 		=> $twitter_username . ':' . $twitter_password,
-    		CURLOPT_POST			=> true,
-    		CURLOPT_POSTFIELDS		=> array(
-    	        'status'	=> $text,
-    			'source'	=> 'noserub'
-    		),
-    		CURLOPT_RETURNTRANSFER	=> true,
-    		CURLOPT_FAILONERROR		=> true,
-    		CURLOPT_HEADER			=> false,
-    		CURLOPT_USERAGENT		=> Configure::read('noserub.user_agent'),
-    		CURLOPT_CONNECTTIMEOUT	=> 120,  
-    		CURLOPT_TIMEOUT			=> 120,
-    		
-    		# Twitter is strict about accepting invalid "Expect" headers
-            CURLOPT_HTTPHEADER      => array('Expect:')
-    	);
-
-        $safe_mode = ini_get('safe_mode');
-        $open_basedir = ini_get('open_basedir');
-        if(!$safe_mode && !$open_basedir) {
-            # this option only works when safe_mode or open_basedir are not enabled
-            $options[CURLOPT_FOLLOWLOCATION] = true;
-            $options[CURLOPT_MAXREDIRS]      = 5; # just randomly picked to restrict it somehow
-        }
+        $key = $data['TwitterAccount']['access_token_key'];
+        $secret = $data['TwitterAccount']['access_token_secret'];
+        $url = 'http://www.twitter.com/statuses/update.json';
         
-        # ignoring all error messages or return values...
-    	$ch = curl_init($uri);
-        curl_setopt_array($ch, $options);
-        $data = curl_exec($ch);
-        $error_msg = curl_error($ch);
+        App::import('Vendor', 'oauth-consumer'.DS.'oauth_consumer');
+        $consumer = new OAuth_Consumer(Context::read('network.twitter_consumer_key'), Context::read('network.twitter_consumer_secret'));
+        $result = $consumer->post($key, $secret, $url, array('status' => $text));
         
         $success = true;
         
-        if($error_msg) {
-            $this->log('cURL error (' . $error_msg . ') when trying to send msg to twitter for ' . $twitter_username, LOG_DEBUG);
+		if(!$result) {
+            $this->log('No data returned by twitter when trying to send update for: ' . $key);
             $success = false;
         }
         
-        if(!$data) {
-            $this->log('No data returned by twitter when trying to send update for:' . $twitter_username);
-            $success = false;
-        }
-        
-        App::import('Vendor', 'json', array('file' => 'Zend'.DS.'Json.php'));
-        $status = Zend_Json::decode($data);
+		App::import('Vendor', 'json', array('file' => 'Zend'.DS.'Json.php'));
+        $status = Zend_Json::decode($result);
         if(!isset($status['id'])) {
-            $this->log('Unexpected data returned by twitter API when trying to send update for: ' . $twitter_username);
-            $this->log(print_r($data, 1), LOG_DEBUG);
+            $this->log('Unexpected data returned by twitter API when trying to send update for: ' . $key);
+            $this->log(print_r($result, 1), LOG_DEBUG);
             $this->log(print_r($status, 1), LOG_DEBUG);
             $success = false;
         }
-        
-        curl_close($ch);
         
         return $success;
 	}
