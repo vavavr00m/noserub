dfaf1077e5509bd6bd6d4f66b6c8c3c48844205c
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-08-15 09:10:18 +0000

    Removing duplication by introducing a factory to create arrays using twitter's data structure

--- app/plugins/api/controllers/statuses_controller.php
+++ app/plugins/api/controllers/statuses_controller.php
@@ -79,43 +79,11 @@ class StatusesController extends ApiAppController {
 	}
 	
 	private function formatStatuses(array $statuses) {
+		App::import('Vendor', 'Api.ArrayFactory');
 		$data = array();
 		
 		foreach ($statuses as $status) {
-			$data[] = array('status' => array('created_at' => date('D M d H:i:s O Y', strtotime($status['Entry']['published_on'])),
-											  'id' => $status['Entry']['id'],
-											  'text' => $status['Entry']['content'],
-											  'source' => '<a href="http://noserub.com">NoseRub</a>', // TODO
-											  'truncated' => 'false', // TODO
-											  'in_reply_to_status_id' => null, // TODO
-  											  'in_reply_to_user_id' => null, // TODO
-  											  'favorited' => 'false', // TODO
-  											  'in_reply_to_screen_name' => null, // TODO
-											  'user' => array('id' => $status['Entry']['identity_id'],
-															  'name' => $status['Identity']['firstname'] . ' ' . $status['Identity']['lastname'], // TODO
-															  'screen_name' => $status['Identity']['single_username'],
-														      'location' => null, // TODO
-														      'description' => null, // TODO
-														      'profile_image_url' => null, // TODO
-														      'url' => null, // TODO
-														      'protected' => 'false',
-														      'followers_count' => null, // TODO
-														      'profile_background_color' => null, // not supported by NoseRub
-														      'profile_text_color' => null, // not supported by NoseRub
-														      'profile_link_color' => null, // not supported by NoseRub
-														      'profile_sidebar_fill_color' => null, // not supported by NoseRub
-														      'profile_sidebar_border_color' => null, // not supported by NoseRub
-														      'friends_count' => null, // TODO
-														      'created_at' => null, // TODO
-														      'favourites_count' => null, // TODO
-														      'utc_offset' => null, // TODO
-														      'time_zone' => null, // TODO
-														      'profile_background_image_url' => null, // not supported by NoseRub
-														      'profile_background_tile' => 'false', // not supported by NoseRub
-														      'statuses_count' => null, // TODO
-														      'notifications' => null, // TODO
-														      'verified' => 'false', // we don't have verified users
-														      'following' => null))); // TODO
+			$data[] = ArrayFactory::status_with_user($status);
 		}
 		
 		return $data;
