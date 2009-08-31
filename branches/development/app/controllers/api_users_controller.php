d26286ce0b9e0c500307587b80931cda4f08f2b5
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-08-31 06:49:34 +0000

    Implementing functionality to save/update username/password for the API

--- branches/development/app/controllers/api_users_controller.php
+++ branches/development/app/controllers/api_users_controller.php
@@ -1,7 +1,16 @@
 <?php
 
 class ApiUsersController extends AppController {
+	public $uses = array('ApiUser');
 	
 	public function index() {
+		if ($this->data) {
+			if ($this->ApiUser->saveUser($this->data['ApiUser']['username'], $this->data['ApiUser']['password'])) {
+				$this->flashMessage('success', __('The new settings have been saved.', true));
+			}
+		} else {
+			$this->data = $this->ApiUser->findByIdentityId(Context::loggedInIdentityId());
+			unset($this->data['ApiUser']['password']);
+		}
 	}
 }
--- branches/development/app/models/api_user.php
+++ branches/development/app/models/api_user.php
@@ -2,4 +2,46 @@
 
 class ApiUser extends AppModel {
 	public $belongsTo = array('Identity');
+	
+	public $validate = array('username' => array('rule' => array('validateUniqueUsername')),
+							 'password' => array('rule' => array('validateMinLengthOfPassword')));
+	
+	public function validateMinLengthOfPassword($value, $params = array()) {
+		if (strlen($this->data['ApiUser']['password_original']) >= 6) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public function validateUniqueUsername($value, $params = array()) {
+		$value = strtolower($value['username']);
+		$id = null;
+		
+		if (isset($this->data['ApiUser']['id'])) {
+			$id = $this->data['ApiUser']['id'];
+		}
+		
+		return !$this->hasAny(array('ApiUser.username' => $value, 'ApiUser.id !=' => $id));
+	}
+	
+	public function saveUser($username, $password) {
+		if (empty($username) && empty($password)) {
+			return $this->deleteAll(array('ApiUser.identity_id' => Context::loggedInIdentityId()));
+		}
+
+		App::import('Core', 'Security');
+		
+		$apiUser = $this->findByIdentityId(Context::loggedInIdentityId());
+		
+		if ($apiUser) {
+			$data['ApiUser']['id'] = $apiUser['ApiUser']['id'];
+		}
+		
+		$data['ApiUser']['identity_id'] = Context::loggedInIdentityId();
+		$data['ApiUser']['username'] = strtolower($username);
+		$data['ApiUser']['password'] = Security::hash($password, null, true);
+		$data['ApiUser']['password_original'] = $password;
+		return $this->save($data);
+	}
 }
