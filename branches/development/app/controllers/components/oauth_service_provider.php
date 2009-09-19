44e0d2f551d447ee11d2901dad9132ac1079d51d
dhofstet <dhofstet@ae07c2c2-3635-0410-bec2-e7f8933db683>
2009-09-19 06:12:28 +0000

    Deprecating getAccessTokenKeyOrDie() and replacing it with getAccessTokenKey()

--- branches/development/app/controllers/components/oauth_service_provider.php
+++ branches/development/app/controllers/components/oauth_service_provider.php
@@ -3,6 +3,23 @@ App::import('Vendor', 'oauth', array('file' => 'Oauth'.DS.'OAuth.php'));
 
 class OauthServiceProviderComponent extends Object {
 	
+	public function getAccessTokenKey() {
+		$server = $this->getServer();
+		$this->removeUrlParamFromQueryString();
+		
+		try {
+			$request = OAuthRequest::from_request();
+			$server->verify_request($request);
+		} catch (OAuthException $e) {
+			return false;
+		}
+		
+		return $request->get_parameter('oauth_token');
+	}
+	
+	/**
+	 * @deprecated, use getAccessTokenKey() instead
+	 */
 	public function getAccessTokenKeyOrDie() {
 		$server = $this->getServer();
 		$this->removeUrlParamFromQueryString();
